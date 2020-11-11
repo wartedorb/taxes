d88b454e6b8ec57ae2c051c9f67fff7d2c30b2a6
Максим <maxiim114@gmail.com>
2020-11-11 18:56:10 +0500

    Annual income counter

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('1')
+# Cycle counting an annual income.
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format('What is your income for', name_month[month], end=''))
+    income = float(input())
+    annual_income += income
+print('Your annual income is', annual_income, '$')
+
