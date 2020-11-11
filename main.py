3638cccc826019163bb4502796e1109d06bde8c1
Kirill20704 <b.kirill.a.2001@gmail.com>
2020-11-11 19:40:33 +0700

    v2

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-tp=int(input('Если вы одиноки, введите 1. Если у Вас есть супруг(а), введите 2. Если у Вас есть дети, но нет супруга(и), введите 3.'))
-tpr=int(input('Введите сумму налогового вычета'))
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 
-if tp==1 :
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format('What is your income for', name_month[month], end=''))
+    income = float(input())
+    annual_income += income
+print('Your annual income is', annual_income, '$')
+tax_deduction = float(input('What is your tax deduction? '))
+an_income = annual_income - tax_deduction
+status=int(input('If you are single, enter 1. If you have a spouse, enter 2. If you have children but no spouse, enter 3.'))
+
+if status==1 :
     a=9075
     b=36900
     c=89350
     d=186350
     e=405100
     f=406750
-elif tp==2 :
+elif status==2 :
     a = 9075*2
     b = 36900*2
     c = 148850
     d = 226850
     e = 405100
     f = 457600
-elif tp==3 :
+elif status==3 :
     a = 12950
     b = 49400
     c = 127550
